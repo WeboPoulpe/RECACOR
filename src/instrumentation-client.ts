932d196c9cc26b01e803d84e424ecef1fc0b6826
Redouan Elmansouri <redouanelmansouri34@gmail.com>
2026-08-16 06:28:21 -0400

    chore: filter Meta Android bridge errors

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -13,6 +13,8 @@ if (dsn) {
       const errorMessage = exception?.value ?? event.message ?? "";
       const embeddedBrowserBridgeErrors = [
         "Error invoking postMessage: Java object is gone",
+        "Error invoking postMessage: Java exception was raised during method invocation",
+        "Error invoking postMessage: Java bridge method invocation error",
         "Error invoking enableDidUserTypeOnKeyboardLogging: Java object is gone",
         "undefined is not an object (evaluating 'window.webkit.messageHandlers')",
       ];
